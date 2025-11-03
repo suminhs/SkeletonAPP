c116ae65d8bb54368867969c3e9e9b4c0e4e50fc
Sofía Pérez <sofiarp14@gmail.com>
2025-11-03 00:56:38 -0300

    Actualizacion de login y home
    NO funciona el calendario...

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  user: string = '';
+  nombre: string = '';
+  apellido: string = '';
+  nivel: string = '';
+  fechaNacimiento: string = '';
+  mostrarCalendario: boolean = false;
 
+  constructor(private alertController: AlertController) {
+    const nav = history.state;
+    this.user = nav.user || 'Invitado';
+    }
+
+  limpiarCampos() {
+    this.nombre = '';
+    this.apellido = '';
+    this.nivel = '';  
+  }
+
+  abrirCalendario() {
+    this.mostrarCalendario = true;
+  }
+
+  cerrarCalendario() {
+    this.mostrarCalendario = false;
+  }
+
+
+  async mostrarDatos() {
+    const alert = await this.alertController.create({
+      header: 'Usuario',
+      message: `Su nombre es ${this.nombre} ${this.apellido}`,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
   standalone: false
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  usuario: string = '';
+  password: string = '';
 
-  ngOnInit() {
+  constructor(private alertController: AlertController,
+              private router: Router    
+  ) { }
+
+// Método para mostrar alerta de error
+async mostrarAlerta(mensaje: string) {
+  const alert = await this.alertController.create({
+    header: 'Error',
+    message: mensaje,
+    buttons: ['OK']
+  });
+  await alert.present();
+}
+  
+
+login() {
+  // Verificación de usuario
+  const usuarioValido = /^[a-zA-Z0-9]{3,8}$/.test(this.usuario);
+  if (!this.usuario) {
+    this.mostrarAlerta('El campo de usuario no puede estar vacío.');
+    return;
   }
+
+  // Validación de formato en correo
+  if (!usuarioValido) {
+    this.mostrarAlerta('El usuario debe ser alfanumérico y tener entre 3 y 8 caracteres.');
+    return;
+  }
+
+  // Verificación de contraseña
+  if (!this.password) {
+    this.mostrarAlerta('El campo de contraseña no puede estar vacío.');
+    return;
+  }
+
+  // Verificación de contraseña con 4 números
+  if (this.password.length !== 4 || !/^\d{4}$/.test(this.password)) {
+    this.mostrarAlerta('La contraseña debe tener 4 números.');
+    return;
+  }
+
+  this.router.navigate(['/home'], { state: { user:this.usuario }});
+}
 
 }
